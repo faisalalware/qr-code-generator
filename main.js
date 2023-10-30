0277f5605a7014fc55d06accff1d387134ce7342
Faisal Alware <85944127+faisalalware@users.noreply.github.com>
2023-10-30 17:08:41 +0530

    code correction

--- main.js
+++ main.js
@@ -1,16 +1,19 @@
+// Get elements from document
 const input = document.getElementById("qr-input");
 const button = document.getElementById("qr-btn");
 const image = document.getElementById("qr-image");
 
-const urlPattern = new RegExp('(?:https?):\/\/(\w+:?\w*)?(\S+)(:\d+)?(\/|\/([\w#!:.?+=&%!\-\/]))?');
-
+// Click event listener on Button
 button.addEventListener('click', function () {
+    // Get url from input
     const inputValue = input.value;
 
+    // Check if input is empty
     if (!inputValue) {
         alert("Please enter URL");
         return;
     }
+    // Generate QR Code
     else {
         image.setAttribute("style", "display: block")
         image.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${inputValue}`;
